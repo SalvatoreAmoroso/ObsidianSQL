2ba51aced728abc29a5cbb1cba4d48d11f606fa8
Salvatore Amoroso <amo_salvatore@web.de>
2021-04-15 18:53:11 +0200

    Add Mockup Classes

---
+++ ObsidianSQL.library.mockup/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsidianSQL.library.mockup
+{
+    public class Connection : IConnection
+    {
+        public IDatabase[] Databases { get; set; }
+
+        public void Connect()
+        {
+            Console.WriteLine($"Connected from DB.");
+        }
+
+        public void Disconnect()
+        {
+            Console.WriteLine($"Disconnected from DB.");
+        }
+
+        public int ExecuteQuery(string query)
+        {
+            Console.WriteLine($"{query} executed.");
+            return 0;
+        }
+
+
+        public Connection()
+        {
+            Databases = new Database[] { new Database("onlineshop"), new Database("onlinegame") };
+        }
+    }
+}
---
+++ ObsidianSQL.library.mockup/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsidianSQL.library.mockup
+{
+    public class Database : IDatabase
+    {
+        public string Name { get; set; }
+
+        public ITable[] Tables { get; set; }
+
+        public void ExecuteQuery(string query)
+        {
+            Console.WriteLine($"{query} executed.");
+        }
+
+        public Database(string name)
+        {
+            Name = name;
+            Tables = new Table[] { new Table("Users"), new Table("Presidents") };
+        }
+    }
+}
