84bdd56ce63565ef822ffdd78a5a7142c9e6d91d
Marco Hannich <marco.hannich@outlook.de>
2021-05-29 14:20:46 +0200

    Add add and remove methods for Databases and Tables

--- ObsidianSQL.library/IConnection.cs
+++ ObsidianSQL.library/IConnection.cs
@@ -3,6 +3,8 @@ namespace ObsidianSQL.library
     public interface IConnection
     {
         public IDatabase[] Databases { get; }
+        public void AddDatabase(IDatabase database);
+        public void RemoveDatabase(IDatabase database);
         public void Connect();
         public void Disconnect();
         public int ExecuteQuery(string query);
--- ObsidianSQL.library/IDatabase.cs
+++ ObsidianSQL.library/IDatabase.cs
@@ -4,6 +4,8 @@ namespace ObsidianSQL.library
     {
         public string Name { get; set; }
         public ITable[] Tables { get; }
+        public void AddTable(ITable table);
+        public void RemoveTable(ITable table);
 
         public void ExecuteQuery(string query);
     }
