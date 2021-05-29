27ef94ff5c93e72452a03b6d99f5017895dfc2ec
Salvatore Amoroso <amo_salvatore@web.de>
2021-05-30 00:51:00 +0200

    Update Exceptions

--- ObsidianSQL.server/src/exceptions/ResourceNotFoundException.cs
+++ ObsidianSQL.server/src/exceptions/ResourceNotFoundException.cs
@@ -2,9 +2,20 @@ using System;
 
 namespace ObsidianSQL.server.src.exceptions
 {
-	public class ResourceNotFoundException : Exception
-	{
-		public ResourceNotFoundException(string message) : base(message) {}
-		public ResourceNotFoundException(string message, Exception inner) : base(message, inner) {} 
-	}
+    public class ResourceNotFoundException : Exception
+    {
+        public ResourceNotFoundException()
+        {
+        }
+
+        public ResourceNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public ResourceNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
 }
